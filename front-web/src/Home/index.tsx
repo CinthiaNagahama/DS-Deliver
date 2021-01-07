3eab15cf7b3944e5af79592e92023721cd3efdc2
Cinthia M. N. Ungefehr <ciin.nagahama@gmail.com>
2021-01-07 18:04:55 -0300

    Adding Routes and Orders Header

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,6 +1,7 @@
 import './styles.css';
 import { ReactComponent as MainImage } from "./Main.svg";
 import Footer from '../Footer';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
@@ -12,21 +13,21 @@ function Home() {
               Faça seu pedido <br />
               que entregamos <br />
               pra você!!!
-          </h1>
+            </h1>
             <h3 className="home-subtitle">
               Escolha o seu pedido e em poucos minutos <br />
               levaremos na sua porta
-          </h3>
-            <a href="orders" className="home-btn-order">
+            </h3>
+            <Link to="orders" className="home-btn-order">
               FAZER PEDIDO
-          </a>
+            </Link>
           </div>
           <div className="home-image">
             <MainImage />
           </div>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </>
   );
 }
